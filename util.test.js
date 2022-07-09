d13ced1eccfc5debc06562b837b62d5ba2d1dc4d
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-07-09 17:29:53 +0700

    Implement a simple integration test for checkAndGenerate() function in util.js

--- util.test.js
+++ util.test.js
@@ -1,5 +1,6 @@
-const { generateText } = require('./util');
+const { generateText, checkAndGenerate } = require('./util');
 
+// unit test 1
 test('should output name and age with a correct format', () => { 
   const text = generateText("Rico", 24);
   expect(text).toBe("Rico (24 years old)");
@@ -8,7 +9,14 @@ test('should output name and age with a correct format', () => {
   expect(text2).toBe("Amma (25 years old)");
 });
 
+// unit test 2
 test('should output data-less text', () => {
   const text = generateText("", null);
   expect(text).toBe(" (null years old)");
 });
+
+// integration test -- Other functions are being called in checkAndGenerate()
+test('should generate a valid text output', () => {
+  const text = checkAndGenerate("Rico", 24);
+  expect(text).toBe("Rico (24 years old)");
+})
