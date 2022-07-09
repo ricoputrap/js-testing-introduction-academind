7d1eb73c214f765135da7e49d9ec55f0529a40a9
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-07-09 17:43:19 +0700

    Implement a simple e2e test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 // unit test 1
@@ -19,4 +20,26 @@ test('should output data-less text', () => {
 test('should generate a valid text output', () => {
   const text = checkAndGenerate("Rico", 24);
   expect(text).toBe("Rico (24 years old)");
+});
+
+test('should click around', async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+    args: ["--window-size=1920,1080"]
+  });
+
+  const page = await browser.newPage();
+  await page.goto(
+    "file:///Users/ricoputrapradana/LEARN/testing/js-testing-introduction-academind/index.html"
+  );
+
+  await page.click("input#name");
+  await page.type("input#name", "Rico");
+
+  await page.click("input#age");
+  await page.type("input#age", '24');
+
+  await page.click("#btnAddUser");
+
 })
